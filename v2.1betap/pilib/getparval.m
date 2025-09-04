279f4306c9ba6a4f57a8f25053c3c0fe0b6839dd
eemne eemne <eemne@login1.aire.lee.alces.network>
2025-09-04 19:09:18 +0100

    updating getparval for R2023 version

--- v2.1betap/pilib/getparval.m
+++ v2.1betap/pilib/getparval.m
@@ -1,19 +1,27 @@
 function [val] = getparval(cfgcell,parstring,defval,indn)
 %% getparval ==============================================================
 % cfgcell: n-by-2 cell array containing parameters
-% parstring: name of desired parameter
+% parstring: name of desired parameter (without colon)
 % defval: default val if parameter is not found
 % indn: index to use when multiple matches are found
 % Andrew Watson @ Leeds, 15/06/2021
-% =========================================
-    if nargin ~= 4
+% Modified: make tolerant to trailing colon in parstring or cfgcell
+% ========================================================================
+    if nargin < 4
         indn = 1;
     end
     
     try
-%         val = cfgcell{strcmp(cfgcell(:,1), parstring),2};
-        indx = find(strcmp(cfgcell(:,1), parstring));
-        val = cfgcell{indx(indn),2};
+        % normalize both sides: strip trailing colon(s)
+        keys = regexprep(cfgcell(:,1), ':$', '');
+        query = regexprep(parstring, ':$', '');
+        
+        indx = find(strcmpi(keys, query));  % case-insensitive match
+        if isempty(indx)
+            val = defval;
+        else
+            val = cfgcell{indx(indn),2};
+        end
     catch
         val = defval;
     end
